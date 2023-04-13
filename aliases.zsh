7c4939c1a31c6baf6b6b0ae641e9641aa3237394
Jeremy Johnson <modernclassicgamer@gmail.com>
2023-04-13 21:51:08 +0000

    update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,10 @@
 alias reload!='. ~/.zshrc'
 alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..'
 alias l.='ls -d .* --color=auto'
 alias ls='ls -lah'
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"

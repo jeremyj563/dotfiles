ae16d39b13aa7add4ce4789a74f689bb62b1b0fd
Jeremy Johnson <modernclassicgamer@gmail.com>
2023-04-13 21:18:44 +0000

    update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -3,3 +3,6 @@ alias ls='ls -lah'
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 alias c='clear'
 alias statc='stat -c %a'
+alias psw='source $HOME/source/repos.personal/jeremyj563/proxy-switcher/proxy-switcher.sh'
+alias proxy_on='psw $PROXY'
+alias proxy_off='psw'
